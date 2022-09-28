7ce63667a643ca600728ca7aac22f6b6d1d4b7be
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-09-29 00:02:49 +0200

    created pages

---
+++ src/pages/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const aboutPage = () => {
+  return (
+    <>
+      <h1>About</h1>
+    </>
+  );
+};
+
+export default aboutPage;
---
+++ src/pages/experience.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const experiencePage = () => {
+  return (
+    <>
+      <h1>ExperiencePage</h1>
+    </>
+  );
+};
+
+export default experiencePage;
