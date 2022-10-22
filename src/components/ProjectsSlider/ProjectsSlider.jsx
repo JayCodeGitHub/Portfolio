3221b3f8098cdda09b4c6be68197402ada8677c4
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-10-22 18:01:50 +0200

    Styled ProjectsSlider for Desktop

--- src/components/ProjectsSlider/ProjectsSlider.jsx
+++ src/components/ProjectsSlider/ProjectsSlider.jsx
@@ -1,10 +1,63 @@
 import React from 'react';
-import { Wrapper } from './ProjectsSlider.styles';
+import {
+  Wrapper,
+  Header,
+  ProjectsWrapper,
+  Project,
+  IconsWrapper,
+} from './ProjectsSlider.styles';
+import { Button } from '../Button/Button.styles';
+import GitHubIcon from '../../assets/icons/Github.svg';
+import LinkIcon from '../../assets/icons/Link.svg';
+import ChampionAcademyImageMobile from '../../assets/images/champion-academy-mobile.png';
+import KarateTeamImageMobile from '../../assets/images/karate-team-mobile.png';
+import HappyNotesImageMobile from '../../assets/images/happy-notes-mobile.png';
 
 const ProjectsSlider = () => {
   return (
     <Wrapper>
-      <h1>Projects Slider</h1>
+      <Header>
+        <h1>Some of my projects</h1>
+        <Button> See More</Button>
+      </Header>
+      <ProjectsWrapper>
+        <Project>
+          <img src={ChampionAcademyImageMobile} />
+          <h1>Champion-Academy</h1>
+          <IconsWrapper>
+            <a target="_blank">
+              <GitHubIcon />
+            </a>
+            <a target="_blank">
+              <LinkIcon />
+            </a>
+          </IconsWrapper>
+        </Project>
+        <Project>
+          <img src={KarateTeamImageMobile} />
+          <h1>Karate Team Oborniki</h1>
+          <IconsWrapper>
+            <a target="_blank">
+              <GitHubIcon />
+            </a>
+            <a target="_blank">
+              <LinkIcon />
+            </a>
+          </IconsWrapper>
+        </Project>
+        <Project>
+          <img src={HappyNotesImageMobile} />
+          <h1>Karate Team Oborniki</h1>
+          <IconsWrapper>
+            <a target="_blank">
+              <GitHubIcon />
+            </a>
+            <a target="_blank">
+              <LinkIcon />
+            </a>
+          </IconsWrapper>
+        </Project>
+      </ProjectsWrapper>
     </Wrapper>
   );
 };
--- src/components/ProjectsSlider/ProjectsSlider.styles.js
+++ src/components/ProjectsSlider/ProjectsSlider.styles.js
@@ -1,7 +1,37 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    h1 {
-        color: red;
+    width: 100%;
+    margin: 10rem 0;
+`
+
+export const Header = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 5rem 0;
+`
+
+export const ProjectsWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+`
+
+export const Project = styled.div`
+    width: 25%;
+    height: 100%;
+    img {
+        width: 100%;
+        border-radius: 15px;
+    }
+`
+
+export const IconsWrapper = styled.div`
+    svg {
+        height: 30px;
+        fill: ${({ theme }) => theme.white};
+        cursor: pointer;
     }
 `
