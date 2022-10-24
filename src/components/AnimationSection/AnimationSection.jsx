a83040fd5798c9453ee1cdac9697992109073684
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-10-24 18:05:26 +0200

    Conected AnimationSection with styles

--- src/components/AnimationSection/AnimationSection.jsx
+++ src/components/AnimationSection/AnimationSection.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Wrapper } from './AnimationSection.styles';
 
 const AnimationSection = () => {
   return (
-    <>
+    <Wrapper>
       <h1>Animation Section</h1>
-    </>
+    </Wrapper>
   );
 };
 
