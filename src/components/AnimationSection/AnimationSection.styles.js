26a928c6136d1dd9f4328cf5e61642409de57112
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-10-26 13:52:11 +0200

    Styled AnimateSection for Mobile

--- src/components/AnimationSection/AnimationSection.styles.js
+++ src/components/AnimationSection/AnimationSection.styles.js
@@ -5,31 +5,43 @@ export const Wrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
+    margin: 10rem 0;
     h1 {
-      margin: 2.5rem 0;
-      width: 100%;
-      font-size: ${({ theme }) => theme.font.size.m};
-   }
-   p {
-      color: ${({ theme }) => theme.grey};
-      width: 100%;
-      font-size: ${({ theme }) => theme.font.size.s};
-   }
-   div {
-        width: 50%;
+        margin: 2.5rem 0;
+        width: 100%;
+        font-size: ${({ theme }) => theme.font.size.m};
+    }
+    p {
+        color: ${({ theme }) => theme.grey};
+        width: 100%;
+        font-size: ${({ theme }) => theme.font.size.s};
+    }
+    div {
+        width: 100%;
         display: flex;
         justify-content: center;
         align-items: space-between;
         flex-direction: column;
+    }
+    ${({ theme }) => theme.mq.desktop} {
+        flex-direction: row;
+        div {
+            width: 50%;
+        }
    }
 `
 
 export const SvgWrapper = styled.div`
     svg {
         width: 100%;
+        height: auto;
     }
 `
 
 export const DescriptionWrapper = styled.div`
     width: 50%;
+    ${({ theme }) => theme.mq.desktop} {
+        width: 100%;
+   }
 `
