332682956744912703cb668a4c69c55296ee9748
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-11-23 02:09:57 +0100

    Created About.styles

---
+++ src/assets/styles/pages/about.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.main`
+    padding: 2.5vw;
+    background-color: ${({ theme }) => theme.tertiary};
+    color: ${({ theme }) => theme.white};
+    ${({ theme }) => theme.mq.bigDesktop} {
+        padding: 2.5vw 15vw;
+    }
+`
+
+export const Paragraph = styled.p`
+    color: red;
+`
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { MainTemplate } from '../templates/MainTemplate';
 import Hero from '../components/Hero/Hero';
+import { Wrapper, Paragraph } from '../assets/styles/pages/about.styles';
 
 const aboutPage = () => {
   return (
     <MainTemplate>
       <Hero secondary title="about" />
+      <Wrapper>
+        <Paragraph>HIeworjbnrwob</Paragraph>
+      </Wrapper>
     </MainTemplate>
   );
 };
