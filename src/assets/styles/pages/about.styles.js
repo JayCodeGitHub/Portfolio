f1d7bd15b8fa0108cfeab65131254ca5ae2fea71
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-12-11 02:10:49 +0100

    Stled About page for Desktop

--- src/assets/styles/pages/about.styles.js
+++ src/assets/styles/pages/about.styles.js
@@ -9,6 +9,48 @@ export const Wrapper = styled.main`
     }
 `
 
+export const StyledSection = styled.section`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`
+
+export const ImageWrapper = styled.div`
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const StyledImage = styled.img`
+    width: 75%;
+    border-radius: 1.5rem;
+`
+
+export const DescriptionWrapper = styled.div`
+    width: 50%;
+`
+
+export const Title = styled.h2`
+    font-size: ${({ theme }) => theme.font.size.m};
+`
+
 export const Paragraph = styled.p`
-    color: red;
+    font-size: ${({ theme }) => theme.font.size.s};
+    color: ${({ theme }) => theme.grey};
+    width: 90%;
+    padding-left: 2rem;
+`
+
+export const StyledTechnologies = styled.ul`
+    width: 90%;
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+    padding: 0;
+`
+
+export const Technologie = styled.li`
+    padding: 0.5rem 1rem;
+    color: ${({ theme }) => theme.primary};
 `
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,14 +1,59 @@
 import React from 'react';
 import { MainTemplate } from '../templates/MainTemplate';
 import Hero from '../components/Hero/Hero';
-import { Wrapper, Paragraph } from '../assets/styles/pages/about.styles';
+import {
+  Wrapper,
+  StyledSection,
+  ImageWrapper,
+  DescriptionWrapper,
+  StyledImage,
+  Title,
+  Paragraph,
+  StyledTechnologies,
+  Technologie,
+} from '../assets/styles/pages/about.styles';
+import ProfileImage from '../assets/images/profile.jpeg';
 
 const aboutPage = () => {
   return (
     <MainTemplate>
       <Hero secondary title="about" />
       <Wrapper>
-        <Paragraph>HIeworjbnrwob</Paragraph>
+        <StyledSection>
+          <DescriptionWrapper>
+            <Title>About Me</Title>
+            <Paragraph>
+              Hello! My name is Brittany and I enjoy creating things that live
+              on the internet. My interest in web development started back in
+              2012 when I decided to try editing custom Tumblr themes — turns
+              out hacking together a custom reblog button taught me a lot about
+              HTML & CSS!
+            </Paragraph>
+            <Paragraph>
+              Fast-forward to today, and I’ve had the privilege of working at an
+              advertising agency, a start-up, a huge corporation, and a
+              student-led design studio. My main focus these days is building
+              accessible, inclusive products and digital experiences at
+              Upstatement for a variety of clients.
+            </Paragraph>
+            <Title>My Skills</Title>
+            <StyledTechnologies>
+              <Technologie>React</Technologie>
+              <Technologie>JavaScript ES6+</Technologie>
+              <Technologie>HTML 5</Technologie>
+              <Technologie>CSS</Technologie>
+              <Technologie>Tailwind CSS</Technologie>
+              <Technologie>TypeSctipt</Technologie>
+              <Technologie>Node.js</Technologie>
+              <Technologie>GraphQL</Technologie>
+              <Technologie>Webpack</Technologie>
+              <Technologie>Docker</Technologie>
+            </StyledTechnologies>
+          </DescriptionWrapper>
+          <ImageWrapper>
+            <StyledImage src={ProfileImage} />
+          </ImageWrapper>
+        </StyledSection>
       </Wrapper>
     </MainTemplate>
   );
