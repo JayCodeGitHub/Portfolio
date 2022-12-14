c112967b69472ac69a79fef8662e4fe09f478c7e
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-12-14 01:48:53 +0100

    Added Helmet

---
+++ src/components/SEO/SEO.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+const SEO = () => (
+  <Helmet>
+    <html lang="en" />
+    <meta name="description" content="test" />
+  </Helmet>
+);
+
+export default SEO;
--- src/templates/MainTemplate.jsx
+++ src/templates/MainTemplate.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '../assets/styles/GlobalStyles';
 import { theme } from '../assets/styles/theme';
+import SEO from '../components/SEO/SEO';
 import Navigation from '../components/Navigation/Navigation';
 import Footer from '../components/Footer/Footer';
 
@@ -9,6 +10,7 @@ export const MainTemplate = ({ children }) => (
   <ThemeProvider theme={theme}>
     <>
       <GlobalStyle />
+      <SEO />
       <Navigation />
       {children}
       <Footer />
